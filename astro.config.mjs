9e18e00c1d756053703994802452e48c6fb96a32
Federico Maria Morrone <contact@morrone.dev>
2024-11-01 18:04:15 +0100

    Add site to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://docs.comfycast.moe",
   integrations: [
     starlight({
       title: "My Docs",
